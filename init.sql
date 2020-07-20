c0e85026f6f8997315cc1ebaae84271e90651cdd
Christopher Medlin <christopherjmedlin@protonmail.com>
2020-07-20 16:31:43 -0600

    Add SQL schema and incomplete PropertyQueries class.
    Signed-off-by: Christopher Medlin <christopherjmedlin@protonmail.com>

---
+++ init.sql
@@ -0,0 +1,17 @@
+-- property id is defined by user after creating a new property, and is thus not auto incremented
+CREATE TABLE properties (
+    id VARCHAR(10) NOT NULL,
+    balance INT,
+    price INT,
+    moveIn DATE,
+    description VARCHAR(500),
+    PRIMARY KEY (id)
+);
+
+-- no clue why auto incrementing is so absurdly verbose in derby
+CREATE TABLE tenants (
+    id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),
+    name VARCHAR(50),
+    email VARCHAR(50),
+    phone VARCHAR(50)
+);
